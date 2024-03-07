6f5bfe558e14809efac4d3535fbb81c9bbb83b27
NPhungDung <dungnpph25823@fpt.edu.vn>
2024-03-07 19:52:15 +0700

    Done bt2

---
+++ src/views/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World Tôi là Dũng nè</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
